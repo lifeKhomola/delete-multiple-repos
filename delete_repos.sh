9226233a864e669dcd7802c22f1b2a6fee881ebb
lifekhomola <khomolavhutshilo5@gmail.com>
2024-01-02 15:54:37 +0200

    Access Token config

--- delete_repos.sh
+++ delete_repos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while IFS= read -r repo; do
-  curl -X DELETE -H "Authorization: token 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" "https://api.github.com/repos/lifeKhomola/$repo"
+  curl -X DELETE -H "Authorization: token YOUR_ACCESS_TOKEN" "https://api.github.com/repos/lifeKhomola/$repo"
 done < repos.txt
